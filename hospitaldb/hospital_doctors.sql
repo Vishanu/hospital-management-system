5d72b78e28fd9112a02ac0775648d4232078ad20
Vishanu Giri <93480207+Vishanu@users.noreply.github.com>
2026-04-27 12:04:37 +0530

    Update hospital_doctors.sql

--- hospitaldb/hospital_doctors.sql
+++ hospitaldb/hospital_doctors.sql
@@ -31,7 +31,7 @@ CREATE TABLE `doctors` (
   PRIMARY KEY (`id`),
   KEY `fk_user` (`user_id`),
   CONSTRAINT `fk_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
