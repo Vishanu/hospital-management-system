77dd3a399fe4674d16bc59f194f50fb5b2c19690
Vishanu Giri <93480207+Vishanu@users.noreply.github.com>
2026-04-27 12:05:01 +0530

    Reset AUTO_INCREMENT value for hospital_patients table

--- hospitaldb/hospital_patients.sql
+++ hospitaldb/hospital_patients.sql
@@ -31,7 +31,7 @@ CREATE TABLE `patients` (
   `disease` varchar(255) DEFAULT NULL,
   `user_id` int DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
